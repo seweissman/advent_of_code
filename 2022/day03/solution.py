db62147991475d4379916e43486276e0e6bdc956
Sarah Weissman <sweissman@stsci.edu>
2022-12-03 11:32:16 -0500

    day3 part 2

--- 2022/day03/solution.py
+++ 2022/day03/solution.py
@@ -30,33 +30,60 @@ In the above example, the priority of the item type that appears in both compart
 
 Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
 
+--- Part Two ---
+As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+
+The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
+
+Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+And the second group's rucksacks are the next three lines:
+
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+
+Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+
+Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
 
 """
 
-from typing import List, Set
+from typing import List
 
 
-def find_overlap(sack: str) -> str:
-    comp1_str = sack[0 : len(sack) // 2]
-    comp2_str = sack[len(sack) // 2 :]
+def find_overlap(sack_list: List[str]) -> str:
 
-    comp1_set = set(comp1_str)
-    comp2_set = set(comp2_str)
-    overlap_set = comp1_set.intersection(comp2_set)
-    if len(overlap_set) != 1:
-        print(comp1_str, comp2_str, overlap_set)
+    sack_sets = [set(sack) for sack in sack_list]
+    overlap_set = sack_sets[0]
+    for sack_set in sack_sets[1:]:
+        overlap_set = overlap_set.intersection(sack_set)
     assert len(overlap_set) == 1
     overlap_char = list(overlap_set)[0]
     return overlap_char
 
 
+def split_sack(sack: str) -> List[str]:
+    comp1_str = sack[0 : len(sack) // 2]
+    comp2_str = sack[len(sack) // 2 :]
+    return [comp1_str, comp2_str]
+
+
 def test_find_overlap():
-    assert find_overlap("vJrwpWtwJgWrhcsFMMfFFhFp") == "p"
-    assert find_overlap("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL") == "L"
-    assert find_overlap("PmmdzqPrVvPwwTWBwg") == "P"
-    assert find_overlap("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn") == "v"
-    assert find_overlap("ttgJtRGJQctTZtZT") == "t"
-    assert find_overlap("CrZsJsPPZsGzwwsLwLmpwMDw") == "s"
+    assert find_overlap(split_sack("vJrwpWtwJgWrhcsFMMfFFhFp")) == "p"
+    assert find_overlap(split_sack("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")) == "L"
+    assert find_overlap(split_sack("PmmdzqPrVvPwwTWBwg")) == "P"
+    assert find_overlap(split_sack("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn")) == "v"
+    assert find_overlap(split_sack("ttgJtRGJQctTZtZT")) == "t"
+    assert find_overlap(split_sack("CrZsJsPPZsGzwwsLwLmpwMDw")) == "s"
 
 
 def priority(c: str) -> int:
@@ -74,8 +101,13 @@ def test_priority():
     assert priority("s") == 19
 
 
-def find_sum_priorities(sacks: List[str]) -> int:
-    return sum([priority(find_overlap(sack)) for sack in sacks])
+def find_sum_priorities_part1(sacks: List[str]) -> int:
+    return sum([priority(find_overlap(split_sack(sack))) for sack in sacks])
+
+
+def find_sum_priorities_part2(sacks: List[str]) -> int:
+    sack_groups = [sacks[i : i + 3] for i in range(0, len(sacks), 3)]
+    return sum([priority(find_overlap(sack_group)) for sack_group in sack_groups])
 
 
 SAMPLE_INPUT = """vJrwpWtwJgWrhcsFMMfFFhFp
@@ -88,7 +120,8 @@ CrZsJsPPZsGzwwsLwLmpwMDw"""
 
 def test_find_sum_priorities():
     sacks = parse_input(SAMPLE_INPUT)
-    assert find_sum_priorities(sacks) == 157
+    assert find_sum_priorities_part1(sacks) == 157
+    assert find_sum_priorities_part2(sacks) == 70
 
 
 def parse_input(text: str) -> List[str]:
@@ -101,4 +134,7 @@ if __name__ == "__main__":
     with open("input.txt", encoding="utf8") as file_in:
         lines = parse_input(file_in.read())
     # part 1
-    print(find_sum_priorities(lines))
+    print(find_sum_priorities_part1(lines))
+
+    # part 2
+    print(find_sum_priorities_part2(lines))
