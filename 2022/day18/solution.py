e04c9b3687445e90a989cb456709e83c71d9f441
Sarah Weissman <sweissman@stsci.edu>
2022-12-20 19:39:35 -0500

    2022 day 18 part 2

--- 2022/day18/solution.py
+++ 2022/day18/solution.py
@@ -39,7 +39,7 @@ In the larger example above, exactly one cube of air is trapped within the lava 
 
 """
 
-from typing import List, Set, Tuple
+from typing import List, Set, Tuple, Union
 
 Cube = Tuple[int, int, int]
 
@@ -76,10 +76,92 @@ def count_exposed_sides(cube_set: Set[Cube]) -> int:
     return exposed_ct
 
 
+def get_boundaries(cube_set: Set[Cube]) -> Tuple[Cube, Cube]:
+    max_x, max_y, max_z = 0, 0, 0
+    min_x, min_y, min_z = 10000, 10000, 10000
+    for cube in cube_set:
+        x, y, z = cube
+        if x > max_x:
+            max_x = x
+        if y > max_y:
+            max_y = y
+        if z > max_z:
+            max_z = z
+        if x < min_x:
+            min_x = x
+        if y < min_y:
+            min_y = y
+        if z < min_z:
+            min_z = z
+    return (min_x, min_y, min_z), (max_x, max_y, max_z)
+
+
+def find_exterior(test_cube: Cube, cube_set: Set[Cube], seen: Set[Cube], minc: Cube, maxc: Cube) -> bool:
+    """DFS from test_cube to find a cube outside the known boundaries of seen cubes"""
+
+    # If we've seen this cube before, do not test it
+    if test_cube in seen:
+        return False
+    seen.add(test_cube)
+
+    # If the cube is in the cube set then it's not exterior
+    if test_cube in cube_set:
+        return False
+
+    (min_x, min_y, min_z) = minc
+    (max_x, max_y, max_z) = maxc
+    tx, ty, tz = test_cube
+
+    # If we find a cube outside the min/max boundaries of the cube set then it's exterior
+    if not min_x < tx < max_x or not min_y < ty < max_y or not min_z < tz < max_z:
+        return True
+    # If the current cube is connected to another cube that is exterior then it is exterior
+    adj_cubes = get_adjacent(test_cube)
+    for adj_cube in adj_cubes:
+        if find_exterior(adj_cube, cube_set, seen, minc, maxc):
+            return True
+    return False
+
+
+def count_non_interior_exposed_sides(cube_set: Set[Cube]) -> int:
+    minc, maxc = get_boundaries(cube_set)
+    known_exterior = set()
+    known_interior = set()
+    exposed_ct = 0
+    for cube in cube_set:
+        adjacent_cubes = get_adjacent(cube)
+        for adj_cube in adjacent_cubes:
+            if adj_cube not in cube_set:
+                visited_set = set()
+                if adj_cube in known_exterior:
+                    exposed_ct += 1
+                elif adj_cube in known_interior:
+                    continue
+                else:
+                    is_exterior = find_exterior(adj_cube, cube_set, visited_set, minc, maxc)
+                    if is_exterior:
+                        # Any cubes visited while searching from this cube are also exterior cubes
+                        # so keep track of them to avoid unnecessary additional searches
+                        known_exterior = known_exterior.union(visited_set)
+                        exposed_ct += 1
+                    else:
+                        # Any cubes visited while searching from this cube are also exterior cubes
+                        # so keep track of them to avoid unnecessary additional searches
+                        known_interior = known_interior.union(visited_set)
+
+    return exposed_ct
+
+
 def test_part_1():
     input_lines = parse_input(SAMPLE_INPUT)
     cube_set = make_cube_set(input_lines)
     assert count_exposed_sides(cube_set) == 64
+
+
+def test_part_2():
+    input_lines = parse_input(SAMPLE_INPUT)
+    cube_set = make_cube_set(input_lines)
+    assert count_non_interior_exposed_sides(cube_set) == 58
 
 
 SAMPLE_INPUT = """
@@ -113,6 +195,8 @@ def main():
     exposed_ct = count_exposed_sides(cube_set)
     print("Part 1:", exposed_ct)
 
+    non_interior_exposed_ct = count_non_interior_exposed_sides(cube_set)
+    print("Part 2:", non_interior_exposed_ct)
 
 if __name__ == "__main__":
     main()
